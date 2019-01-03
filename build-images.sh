f33065956863ef69369c9af3248a41f95e925080
neel.patel <neelpatel@ubuntu.ubuntu-domain>
2019-01-03 13:11:41 +0530

    changed base images of master worker and submit

--- build-images.sh
+++ build-images.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build -t spark-base:2.3.1 ./docker/base
+#docker build -t spark-base:2.3.1 ./docker/base
 docker build -t spark-master:2.3.1 ./docker/spark-master
 docker build -t spark-worker:2.3.1 ./docker/spark-worker
 docker build -t spark-submit:2.3.1 ./docker/spark-submit
