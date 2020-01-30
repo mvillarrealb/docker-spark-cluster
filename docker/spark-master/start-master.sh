74b489710af4971af6099e456608cdf70fcd1ba4
kkeker <k.keker@ya.ru>
2020-01-30 12:50:09 +0700

    Change Base-image & Update components

--- docker/spark-master/start-master.sh
+++ docker/spark-master/start-master.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-export SPARK_MASTER_HOST=`hostname`
+export SPARK_MASTER_HOST=`cat /etc/hostname`
 
-. "/spark/sbin/spark-config.sh"
+. "$SPARK_HOME/sbin/spark-config.sh"
 
-. "/spark/bin/load-spark-env.sh"
+. "$SPARK_HOME/bin/load-spark-env.sh"
 
 mkdir -p $SPARK_MASTER_LOG
 
-export SPARK_HOME=/spark
-
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+cd $SPARK_HOME/bin && $SPARK_HOME/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
