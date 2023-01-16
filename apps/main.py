020b3c4fba4b910c34b6e08423193dd48c200a9f
thuongle2210 <thuong.le2210@hcmut.edu.vn>
2023-01-16 09:36:25 +0700

    spark read from bigquery and write back to bigquery

--- apps/main.py
+++ apps/main.py
@@ -16,20 +16,24 @@ def main():
     "password": "casa1234",
     "driver": "org.postgresql.Driver"
   }
-  file = "/opt/spark-data/MTA_2014_08_01.csv"
+  file = "/opt/spark-data/B63-2011-04-03_2011-05-03.csv"
   sql,sc = init_spark()
 
   df = sql.read.load(file,format = "csv", inferSchema="true", sep="\t", header="true"
-      ) \
-      .withColumn("report_hour",date_format(col("time_received"),"yyyy-MM-dd HH:00:00")) \
-      .withColumn("report_date",date_format(col("time_received"),"yyyy-MM-dd"))
+      )
+      # .withColumn("report_hour",date_format(col("time_received"),"yyyy-MM-dd HH:00:00")) \
+      # .withColumn("report_date",date_format(col("time_received"),"yyyy-MM-dd"))
   
   # Filter invalid coordinates
-  df.where("latitude <= 90 AND latitude >= -90 AND longitude <= 180 AND longitude >= -180") \
-    .where("latitude != 0.000000 OR longitude !=  0.000000 ") \
-    .write \
-    .jdbc(url=url, table="mta_reports", mode='append', properties=properties) \
-    .save()
-  
+  # df.where("latitude <= 90 AND latitude >= -90 AND longitude <= 180 AND longitude >= -180") \
+  #   .where("latitude != 0.000000 OR longitude !=  0.000000 ") \
+  #   .write \
+  #   .jdbc(url=url, table="mta_reports", mode='append', properties=properties) \
+  #   .save()
+  # df.write \
+  #   .jdbc(url=url, table="mta_reports", mode='append', properties=properties) \
+  #   .save()
+  df.show(3)
+    
 if __name__ == '__main__':
   main()
--- start-spark.sh
+++ start-spark.sh
@@ -7,12 +7,12 @@ then
 
 export SPARK_MASTER_HOST=`hostname`
 
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT #>> $SPARK_MASTER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
 
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER #>> $SPARK_WORKER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "submit" ];
 then
